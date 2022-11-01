4189441830f92b4b7427b2643d764378e22be965
andrew.robertson1 <andrew.robertson@enablo.com>
2022-11-01 12:25:55 +1100

    wip

--- packages/calendar.app/src/routes/+page.server.ts
+++ packages/calendar.app/src/routes/+page.server.ts
@@ -1,3 +1,4 @@
+import { base } from '$app/paths';
 import type { PageServerLoad } from '.svelte-kit/types/src/routes/$types';
 import { redirect } from '@sveltejs/kit';
 import { padStart } from 'lodash-es';
@@ -6,5 +7,5 @@ export const load: PageServerLoad = async () => {
   const today = new Date();
   const year = today.getFullYear().toString();
   const month = padStart((today.getMonth() + 1).toString(), 2, '0');
-  throw redirect(302, `/${year}/${month}`);
+  throw redirect(302, `${base}/${year}/${month}`);
 };
