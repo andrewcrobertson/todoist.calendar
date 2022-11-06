44e3784c36a4bf2c728b5caea4d3415204e8a848
andrew.robertson1 <andrew.robertson@enablo.com>
2022-11-06 20:58:54 +1100

    wip

--- packages/calendar.app/src/routes/[year=integer]/[month=integer]/+page.server.ts
+++ packages/calendar.app/src/routes/[year=integer]/[month=integer]/+page.server.ts
@@ -5,7 +5,7 @@ import { filter } from 'lodash-es';
 export const load: PageServerLoad = async ({ params }) => {
   const { year, month } = <any>params;
   const data = apiService.getInitialData(parseInt(year, 10), parseInt(month, 10));
-  const day = 14;
+  const day = 13;
   const days1 = filter(data.days, (d) => d.value <= day);
   const days2 = filter(data.days, (d) => d.value > day);
   return { ...data, days1, days2 };
