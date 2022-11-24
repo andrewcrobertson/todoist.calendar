44345123e2c7ebb59cfda820fd5e09b6d2c0f486
Lara Robertson <laraejrobertson@gmail.com>
2022-11-25 08:37:54 +1100

    add images for months

--- packages/calendar.app/src/routes/[year=integer]/[month=integer]/+page.svelte
+++ packages/calendar.app/src/routes/[year=integer]/[month=integer]/+page.svelte
@@ -8,11 +8,12 @@
 <div class="flex flex-col flex-wrap w-21 h-29 mx-auto space-y-4 divide-y divide-gray-400">
   <div class="w-45">
     <h1 class="text-4xl font-bold pb-6">{data.title.month} {data.title.year}</h1>
-    <div class="pb-6">
+    <div>
       <Image month={data.title.month} />
     </div>
   </div>
   {#each data.days as day}
     <Day value={day.value} dayName={day.dayName} entries={day.entries} />
   {/each}
+  <div />
 </div>
--- packages/calendar.app/src/routes/[year=integer]/[month=integer]/components/image.svelte
+++ packages/calendar.app/src/routes/[year=integer]/[month=integer]/components/image.svelte
@@ -1,17 +1,17 @@
 <script lang="ts">
   import { lowerCase } from 'lodash-es';
-  import img01Url from '../assets/01.png';
-  import img02Url from '../assets/02.png';
-  import img03Url from '../assets/03.png';
-  import img04Url from '../assets/04.png';
-  import img05Url from '../assets/05.png';
+  import img01Url from '../assets/01.jpg';
+  import img02Url from '../assets/02.jpg';
+  import img03Url from '../assets/03.jpg';
+  import img04Url from '../assets/04.jpg';
+  import img05Url from '../assets/05.jpg';
   import img06Url from '../assets/06.png';
   import img07Url from '../assets/07.png';
-  import img08Url from '../assets/08.png';
-  import img09Url from '../assets/09.png';
-  import img10Url from '../assets/10.png';
+  import img08Url from '../assets/08.jpg';
+  import img09Url from '../assets/09.jpg';
+  import img10Url from '../assets/10.jpg';
   import img11Url from '../assets/11.png';
-  import img12Url from '../assets/12.png';
+  import img12Url from '../assets/12.jpg';
   export let month: string;
 
   const getImageUrl = (month: string) => {
@@ -32,4 +32,4 @@
   const imgUrl = getImageUrl(lowerCase(month));
 </script>
 
-<img class="h-40 mx-auto" src={imgUrl} alt={month} />
+<img class="h-48 mx-auto" src={imgUrl} alt={month} />
