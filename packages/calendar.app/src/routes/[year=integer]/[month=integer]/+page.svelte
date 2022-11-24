0205a61bf32ed2956ab49946bfc6b16f930dd20d
Lara Robertson <laraejrobertson@gmail.com>
2022-11-25 08:59:25 +1100

    styles

--- packages/calendar.app/src/routes/[year=integer]/[month=integer]/+page.svelte
+++ packages/calendar.app/src/routes/[year=integer]/[month=integer]/+page.svelte
@@ -5,7 +5,7 @@
   export let data: PageData;
 </script>
 
-<div class="flex flex-col flex-wrap w-21 h-29 mx-auto space-y-4 divide-y divide-gray-400">
+<div class="flex flex-col flex-wrap w-21 h-29 mx-auto space-y-4 divide-y divide-gray-400 pl-5">
   <div class="w-45">
     <h1 class="text-4xl font-bold pb-6">{data.title.month} {data.title.year}</h1>
     <div>
